d56e9e4e54835022e760e98b177e3c0b52601e9c
Jinyoung <roghabo@gmail.com>
2020-12-08 23:20:19 +0900

    finish course

--- src/movies/movies.service.spec.ts
+++ src/movies/movies.service.spec.ts
@@ -3,10 +3,10 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { MoviesService } from './movies.service';
 import { NotFoundException } from '@nestjs/common';
 
-describe('MoviesService', () => {
+describe('MoviesService', () => { //테스트를 묘사함
   let service: MoviesService;
 
-  beforeEach(async () => {
+  beforeEach(async () => { //테스트하기 전에 실행 됨
     const module: TestingModule = await Test.createTestingModule({
       providers: [MoviesService],
     }).compile();
@@ -14,14 +14,17 @@ describe('MoviesService', () => {
     service = module.get<MoviesService>(MoviesService);
   });
 
+  //afterAll() 안에는 데이터베이스를 깨끗하게 정리해주는(모두 지우는) function을 넣을 수 있음
+  //그 외에 afterEach(), beforAll() 등이 있음
+
   it('should be defined', () => {
-    expect(service).toBeDefined();
+    expect(service).toBeDefined(); //정의되어 있어야 함
   });
 
   describe('getAll', () => {
     it('should return an array', () => {
       const result = service.getAll();
-      expect(result).toBeInstanceOf(Array);
+      expect(result).toBeInstanceOf(Array); //배열이 리턴되어야 함
     });
   });
 
